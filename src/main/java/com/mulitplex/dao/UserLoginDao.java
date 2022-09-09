d2d6dcaaf7cee21518de585f843b413874404516
Hanush Kumar <hhanushkumar@deloitte.com>
2022-09-09 14:29:31 +0530

    first commit

---
+++ src/main/java/com/mulitplex/dao/UserLoginDao.java
@@ -0,0 +1,49 @@
+package com.mulitplex.dao;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+public class UserLoginDao {
+	public void userLogin(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			boolean flag=false;
+			Connection con= DBUtility.getConnection();
+			Statement st = con.createStatement();
+			String emailId1=request.getParameter("EmailId");
+			String password1=request.getParameter("Password");
+			String queryString = "select EmailId, Password from user ";
+	        ResultSet results = st.executeQuery(queryString);
+	        while (results.next()) {
+		        String emailId2 = results.getString("EmailId");
+		        String password2 =  results.getString("Password");
+		        if ((emailId1.equals(emailId2)) && (password1.equals(password2))) {
+		              flag = true;  
+		        } 
+	        }
+	        if(!flag){
+	               JOptionPane.showMessageDialog(null, "Please Check Username and Password ");
+	               RequestDispatcher rd=request.getRequestDispatcher("SignIn.jsp");  
+	               rd.forward(request, response);
+	        }
+	        results.close();
+            st.close();
+            con.close();
+            PrintWriter out = response.getWriter();
+            if(flag) {
+            	out.println("<html><body><b>Login Successful" + "</b></body></html>");
+            }else {
+            	out.println("<html><body><b>Login Failed" + "</b></body></html>");
+            }
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+}
